f2289d96020b1a6b2c5dbadb9b51d99fdbddacbb
Satvik Sharma <satvik@easyagile.com>
2024-05-17 11:39:52 +1000

    Update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,27 @@
-eval (dircolors -c ~/.dircolors)
-#eval (thefuck --alias | tr '\n' ';')
+if test -f ~/.dircolors
+    eval (dircolors -c ~/.dircolors)
+end
 
 set -x PATH ~/bin/ ~/.local/bin ~/.yarn/bin $PATH
 
 set -x BROWSER firefox
 set -x EDITOR nvim
 
-set -x TTC_BOTS tinycarebot,selfcare_bot,magicrealismbot
-set -x TTC_REPOS ~/src
-set -x TTC_APIKEYS false
+#CUSTOM GIT ALIASES
+alias g="git"
+alias ga="git add ."
+alias gap="git add -p"
+alias gb="git checkout -b"
+alias gc="git commit"
+alias gchm="git checkout main"
+alias gch="git checkout"
+alias gf="git fetch"
+alias gph="git push"
+alias gpho="git push -u origin HEAD"
+alias gpl="git pull"
+alias gpm="git pull origin main"
+alias gs="git status"
+    # Deletes all branches locally that have been merged into the current branch
+alias gdmb="git branch --merged | grep -v \"\*\" | grep -v main | grep -v dev | xargs -n 1 git branch -d"
+#Fuzzy find and checkout branch
+    alias gce="git checkout (git branch | fzf)"
